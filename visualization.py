a542fcbdfdb816f253f34430194873c5cb27524c
Benjamin Auer <benjamin@oater.de>
2025-08-19 23:53:06 +0200

    I added a forth figure

--- visualization.py
+++ visualization.py
@@ -72,9 +72,9 @@ def visualize(df: pd.DataFrame, outdir: Path):
     fig1, ax11 = plt.subplots()
 
     # left axis (solid lines)
-    ax11.plot(x, df[ID_CTS], label=ID_CTS, color=colors[0], linestyle="-")
-    ax11.plot(x, df[ID_CW],  label=ID_CW,  color=colors[1], linestyle="-")
-    ax11.plot(x, df[ID_CC],  label=ID_CC,  color=colors[2], linestyle="-")
+    ax11.plot(x, df[ID_CTS], label="$CTS$", color=colors[0], linestyle="-")
+    ax11.plot(x, df[ID_CW],  label="$CW$",  color=colors[1], linestyle="-")
+    ax11.plot(x, df[ID_CC],  label="$CC$",  color=colors[2], linestyle="-")
 
     ax11.set_xlabel("Subscription size (ltr)")
     ax11.set_ylabel("Cost (EUR)")
@@ -83,9 +83,9 @@ def visualize(df: pd.DataFrame, outdir: Path):
 
     # right axis (dashed lines, same colors)
     ax12 = ax11.twinx()
-    ax12.plot(x, df[ID_PROP_CG], label=ID_PROP_CG, color=colors[0], linestyle="--")
-    ax12.plot(x, df[ID_PROP_CW], label=ID_PROP_CW, color=colors[1], linestyle="--")
-    ax12.plot(x, df[ID_PROP_CC], label=ID_PROP_CC, color=colors[2], linestyle="--")
+    ax12.plot(x, df[ID_PROP_CG], label="$\dfrac{CG}{CTS}$", color=colors[0], linestyle="--")
+    ax12.plot(x, df[ID_PROP_CW], label="$\dfrac{CW}{CTS}$", color=colors[1], linestyle="--")
+    ax12.plot(x, df[ID_PROP_CC], label="$\dfrac{CC}{CTS}$", color=colors[2], linestyle="--")
     ax12.set_ylabel("Proportion (%)")
     ax12.set_ylim(0, 100)
 
@@ -102,9 +102,9 @@ def visualize(df: pd.DataFrame, outdir: Path):
     colors = ["tab:blue", "tab:orange", "tab:green"]
 
     # left axis (solid lines)
-    ax2.plot(x, df[ID_RWS], label="relative warehouse savings (%)", color=colors[1], linestyle="--")
-    ax2.plot(x, df[ID_RCS],  label="relative carrier savings (%)",  color=colors[2], linestyle="--")
-    ax2.axhline(y=df[ID_RCS].max(), color="red", linestyle="--", linewidth=1.5, label=f"Upper bound on carrier savings: {df[ID_RCS].max():.2f} (%)")
+    ax2.plot(x, df[ID_RWS], label="$\eta_W$", color=colors[1], linestyle="--")
+    ax2.plot(x, df[ID_RCS],  label="$\eta_C$",  color=colors[2], linestyle="--")
+    ax2.axhline(y=df[ID_RCS].max(), color="red", linestyle="--", linewidth=1.5, label=f"$\max(\eta_C)$ = {df[ID_RCS].max():.2f}")
 
     ax2.set_xlabel("Subscription size (ltr)")
     ax2.set_ylabel("Relative savings (compared to 80 ltr subscription) (%)")
@@ -116,9 +116,9 @@ def visualize(df: pd.DataFrame, outdir: Path):
     # figure 3: levelized CTS per parcel
     fig3, ax31 = plt.subplots()
 
-    ax31.plot(x, df[ID_LPV], label="Levelized parcel value (EUR)", color=colors[0], linestyle="-")
+    ax31.plot(x, df[ID_LPV], label="$LPV$", color=colors[0], linestyle="-")
     # vertical line at subscription size = 110
-    ax31.axhline(y=df[ID_LPV].max(), color="red", linestyle="--", linewidth=1.5, label=f"Upper bound on parcel value: {df[ID_LPV].max():.0f} (EUR/parcel)")
+    ax31.axhline(y=df[ID_LPV].max(), color="red", linestyle="--", linewidth=1.5, label=f"$\max(LPV)$ = {df[ID_LPV].max():.0f}")
 
     ax31.set_xlabel("Subscription size (ltr)")
     ax31.set_ylabel("Parcel value (EUR/parcel)")
@@ -138,6 +138,34 @@ def visualize(df: pd.DataFrame, outdir: Path):
     lines2, labels2 = ax32.get_legend_handles_labels()
     ax31.legend(lines1 + lines2, labels1 + labels2, loc="upper left")
 
+
+    #figure 4: carrier cost
+    cost = np.array([3.62, 4.17, 4.83, 6.3, 6.85, 7.02, 7.29])
+    weight = np.array([1.5, 3.5, 8.5, 13.5, 18.5, 23.5, 30])
+    cost_per_weight = cost/weight
+
+    fig4, ax41 = plt.subplots()
+
+    ax41.plot(weight, cost, label="$SC$", color=colors[0], linestyle="-")
+
+    ax41.set_xlabel("Parcel weight (kg)")
+    ax41.set_ylabel("Parcel cost (EUR)")
+    ax41.set_ylim(0, 10)
+    ax41.grid(True, linestyle=":")
+
+    ax42 = ax41.twinx()
+    ax42.plot(weight, cost_per_weight, label="$\dfrac{SC}{SW}$",color=colors[1])
+
+    ax42.set_ylabel("Cost per weight (EUR/kg)")
+    ax42.set_ylim(0, 3)
+
+    # merge legends from both axes
+    lines1, labels1 = ax41.get_legend_handles_labels()
+    lines2, labels2 = ax42.get_legend_handles_labels()
+    ax42.legend(lines1 + lines2, labels1 + labels2, loc="upper center")
+
+
+
     if SHOW_PLT:
         plt.show()
 
@@ -145,6 +173,7 @@ def visualize(df: pd.DataFrame, outdir: Path):
         fig1.savefig(outdir / "cost-of-species.svg", format="svg", bbox_inches="tight")
         fig2.savefig(outdir / "relative-savings.svg", format="svg", bbox_inches="tight")
         fig3.savefig(outdir / "parcel-value.svg", format="svg", bbox_inches="tight")
+        fig4.savefig(outdir / "V-vector.svg", format="svg", bbox_inches="tight")
 
 def main():
     df = contact_fast(MIN_LTR,MAX_LTR)
